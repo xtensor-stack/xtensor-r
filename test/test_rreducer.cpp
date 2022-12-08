73d3bbb259521555cd6d85b9bc0b043207d81a99
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-12-08 14:53:03 +0100

    Add test for variance bug

--- test/test_rreducer.cpp
+++ test/test_rreducer.cpp
@@ -7,12 +7,14 @@
 * The full license is in the file LICENSE, distributed with this software. *
 ****************************************************************************/
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-#include "xtensor/xarray.hpp"
-#include "xtensor/xmath.hpp"
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xmath.hpp>
 
 #include "xtensor-r/rarray.hpp"
+#include "xtensor-r/rtensor.hpp"
 
 namespace xt
 {
@@ -23,5 +25,25 @@ namespace xt
 
         EXPECT_EQ(x_sum(0), 6);
         EXPECT_EQ(x_sum(1), 15);
+    }
+
+    TEST(rxreducer, variance)
+    {
+        // For computing references
+        xt::xtensor<int, 2> data = {{1, 2, 3}, {4, 5, 6}};
+        std::vector<std::size_t> const axes = {1};
+
+        {  // xt:rarray
+            xt::rarray<int> arr = data;
+            EXPECT_EQ(xt::variance(arr)(0), xt::variance(data)(0));
+            auto var = xt::variance(arr, axes);
+            EXPECT_EQ(xt::variance(arr, axes), xt::variance(data, axes));
+        }
+        {  // xt:rtensor
+            xt::rtensor<int, 2> arr = data;
+            EXPECT_EQ(xt::variance(arr)(0), xt::variance(data)(0));
+            auto var = xt::variance(arr, axes);
+            EXPECT_EQ(xt::variance(arr, axes), xt::variance(data, axes));
+        }
     }
 }
